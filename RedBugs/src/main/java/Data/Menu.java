4043e45d30f998879720c1a44351b9a14425fe32
WladyslawM <uladzislaumikhno@gmail.com>
2023-02-16 23:22:52 +0100

    adding Menu v1.2

--- RedBugs/src/main/java/Data/Menu.java
+++ RedBugs/src/main/java/Data/Menu.java
@@ -1,4 +1,100 @@
 package Data;
 
+import java.util.Scanner;
+
 public class Menu {
+    private static int numMenu;
+    private static int numberExit;
+    private static boolean  in = false;
+
+    public static  void menu() throws Exception, Error {
+        System.out.println(" ");
+        System.out.println("Start Scan!");
+        System.out.println(" ");
+
+        do {
+            try {
+                Scanner scanner = new Scanner(System.in);
+
+                System.out.println("Menu");
+                System.out.println(" ");
+                System.out.println("1. Logowanie");
+                System.out.println("2. Rejestracja");
+                System.out.println("3. Rozkład jazdy");
+                System.out.println("4. Bus stops");
+                System.out.println("5. Exit");
+                System.out.println(" ");
+                System.out.print("Enter number: ");
+
+                numMenu = scanner.nextInt();
+
+                switch (numMenu) {
+                    case 1 :
+                        System.out.println("Enter in Logowanie");
+                        //Metods for Login
+                        returnToMenu();
+                        break;
+                    case 2 :
+                        System.out.println("Enter in Rejestracja");
+                        // Metods for Reastretion
+                        returnToMenu();
+                        break;
+                    case 3 :
+                        System.out.println("Enter in Rozkład jazdy");
+                        // Metods do rozdładu jazdy
+                        //BookExcel.readListBusStop("myFile.xlt");
+                        returnToMenu();
+                        break;
+                    case 4 :
+                        System.out.println("Enter in Bus stops");
+                        // Metods for bus stops!
+                        //BookExcel.readListBusOnStop("myFile.xlt");
+                        returnToMenu();
+                        break;
+                    case 5 :
+                        System.out.println("Exit");
+                        System.out.println("Goodbye!");
+                        // Metods  for exit and goodbay!
+                        break;
+                }
+            } catch (Exception e) {
+                System.out.println("Number can't be lass or equal zero!");
+            } catch (Error b) {
+                System.out.println("Number can't be more 5");
+            }
+        } while (getNumMenu() != 5);
+
+        System.out.println("End Scan!");
+    }
+
+    public static void returnToMenu() throws Exception {
+
+        do {
+            try {
+                Scanner scannerReturn = new Scanner(System.in);
+
+                System.out.print("Enter number 1 for Exit: ");
+
+                numberExit = scannerReturn.nextInt();
+
+                switch (numberExit) {
+                    case 1 :
+
+                        System.out.println("Enter in Menu.....");
+                        //Metods for return menu
+                        break;
+                }
+            } catch (Exception a) {
+                System.out.println("Number must be one!");
+            }
+        } while (getNumberExit() != 1);
+    }
+
+    public static int getNumberExit() {
+        return numberExit;
+    }
+
+    public static int getNumMenu() {
+        return numMenu;
+    }
 }
