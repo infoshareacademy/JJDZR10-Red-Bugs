3fac645f175c440d7e5e7aa3514e47e068831bdf
BartoszBalka <bartosz.balka@wp.pl>
2023-02-19 12:07:50 +0100

    Zajecia 19022023

--- RedBugs/src/main/java/Data/Stop.java
+++ RedBugs/src/main/java/Data/Stop.java
@@ -1,4 +1,41 @@
 package Data;
 
 public class Stop {
+
+    private int id;
+    private String name;
+    private Schedule schedule;
+
+    public Stop(int id, String name, Schedule schedule) {
+        this.id = id;
+        this.name = name;
+        this.schedule = schedule;
+    }
+
+    public Stop() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Schedule getSchedule() {
+        return schedule;
+    }
+
+    public void setSchedule(Schedule schedule) {
+        this.schedule = schedule;
+    }
 }
--- RedBugs/src/main/java/com/infoshareacademy/App.java
+++ RedBugs/src/main/java/com/infoshareacademy/App.java
@@ -1,5 +1,12 @@
 package com.infoshareacademy;
 
+import Data.Schedule;
+import Data.Stop;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static Data.Menu.menu;
 
 /**
@@ -10,6 +17,16 @@ public class App
 {
     public static void main( String[] args ) throws Exception {
         System.out.println( "RedBugs" );
+        Stop stop1 = new Stop(1,"Przystanek1", new Schedule());
+        Stop stop2 = new Stop(2,"Przystanek2", new Schedule());
+        Stop stop3 = new Stop(3,"Przystanek3", new Schedule());
+        List <Stop> stopList= new ArrayList<>();
+        stopList.add(stop1);
+        stopList.add(stop2);
+        stopList.add(stop3);
+        ObjectMapper objectMapper = new ObjectMapper();
+        String stopsAsString = objectMapper.writeValueAsString(stopList);
+
 
         menu();
     }
