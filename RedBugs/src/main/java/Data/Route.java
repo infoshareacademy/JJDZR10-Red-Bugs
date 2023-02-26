29e5918ea4c3f2f44f0b29e39776ede4c4858a34
BartoszBalka <bartosz.balka@wp.pl>
2023-02-26 13:43:26 +0100

    Added creation Stops from JSON file into a list with 6 examples

--- RedBugs/src/main/java/Data/Route.java
+++ RedBugs/src/main/java/Data/Route.java
@@ -45,12 +45,11 @@ public class Route {
         try (Reader reader = new FileReader("stops.txt")){
             List <Stop> stop = gson.fromJson(reader,List.class);
 
-
-            //Path path = Paths.get("stops.txt");
-            //String readedStops = Files.readString(path);
+            //wypisywanie przystankow linia po linii
+            Path path = Paths.get("stops.txt");
+            String readedStops = Files.readString(path);
             System.out.println("All stops taken from the file stops.txt");
-            //System.out.println(readedStops);
-            System.out.println(stop.toString());
+            System.out.println(readedStops);
 
         }
 
@@ -59,33 +58,11 @@ public class Route {
 
     public static void routescreator() throws IOException {
         fromJson();
-        /* - Stara wersja z Object Mapper
-        Stop stop1 = new Stop(1, "Przystanek1", new Schedule());
-        Stop stop2 = new Stop(2, "Przystanek2", new Schedule());
-        Stop stop3 = new Stop(3, "Przystanek3", new Schedule());
-        Stop stop4 = new Stop(4, "Przystanek4", new Schedule());
-        List<Stop> stopList = new ArrayList<>();
-        stopList.add(stop1);
-        stopList.add(stop2);
-        stopList.add(stop3);
-        stopList.add(stop4);
-        ObjectMapper objectMapper = new ObjectMapper();
-        String stopsAsString = objectMapper.writeValueAsString(stopList);
-        System.out.println(stopsAsString);
-        Path path = Paths.get("stops.txt");
-        Files.write(path, stopsAsString.getBytes());
-        String readedStops = Files.readString(path);
-
-        //List<Stop> stopsReadedFromFileAsPojo = objectMapper.readValue(readedStops, new TypeReference<List<Stop>>() {
-        //});
-
-         */
-    }
+     }
 
     public static void routesreader() throws IOException {
         Path path = Paths.get("stops.txt");
         String readedStops = Files.readString(path);
-        System.out.println("All stops list taken from the file stops.txt");
         System.out.println(readedStops);
     }
 }
