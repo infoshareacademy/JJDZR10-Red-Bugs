b7fdf4486890d89b3dba59b18ee9b9cb9995e546
adampawlowski1987 <adampawlowski1987@gmail.com>
2023-03-12 12:42:21 +0100

    Spring Added without changing dependencies second try

---
+++ RedBugs/red-bugs-app/src/main/java/com/infoshareacademy/redbugsapp/RedBugsAppApplication.java
@@ -0,0 +1,13 @@
+package com.infoshareacademy.redbugsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedBugsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedBugsAppApplication.class, args);
+	}
+
+}
