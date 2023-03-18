52aec3ddf7b59aca1c753717444c9f6ce0c09f12
WladyslawM <uladzislaumikhno@gmail.com>
2023-03-19 00:48:14 +0100

    create ReadService and WriteService

--- RedBugs_webapp/src/main/java/com/isa/pl/redbugs/service/ReadService.java
+++ RedBugs_webapp/src/main/java/com/isa/pl/redbugs/service/ReadService.java
@@ -2,9 +2,7 @@ package com.isa.pl.redbugs.service;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.isa.pl.redbugs.model.Route;
-import com.isa.pl.redbugs.model.Stop;
-import com.isa.pl.redbugs.model.Vehicle;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -13,41 +11,13 @@ import java.util.List;
 
 public class ReadService {
 
-    public List<Object> readJson(){
-        //read json generic instatof
-        return null;
-    }
-
-    public List<Stop> readFromJsonStops(String fileName) throws IOException {
+    public List<Object> readJson(String fileName) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
         Path path = Paths.get(fileName);
 
         String readFromJson = Files.readString(path);
 
-        List<Stop> deserializedJson = objectMapper.readValue(readFromJson, new TypeReference<List<Stop>>() {
+        return objectMapper.readValue(readFromJson, new TypeReference<List<Object>>() {
         });
-        return deserializedJson;
-    }
-
-    public List<Vehicle> readFromJsonVehicles(String fileName) throws IOException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        Path path = Paths.get(fileName);
-
-        String readFromJson = Files.readString(path);
-
-        List<Vehicle> deserializedJson = objectMapper.readValue(readFromJson, new TypeReference<List<Vehicle>>() {
-        });
-        return deserializedJson;
-    }
-
-    public List<Route> readFromJsonRoutes(String fileName) throws IOException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        Path path = Paths.get(fileName);
-
-        String readFromJson = Files.readString(path);
-
-        List<Route> deserializedJson = objectMapper.readValue(readFromJson, new TypeReference<List<Route>>() {
-        });
-        return deserializedJson;
     }
 }
