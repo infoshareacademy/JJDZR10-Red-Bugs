b0a38aec7bd98b0c42654b4e67e094a20e9183eb
WladyslawM <uladzislaumikhno@gmail.com>
2023-03-19 00:59:07 +0100

    remake InitDataService

--- RedBugs_webapp/src/main/java/com/isa/pl/redbugs/service/InitDataService.java
+++ RedBugs_webapp/src/main/java/com/isa/pl/redbugs/service/InitDataService.java
@@ -4,15 +4,13 @@ import com.isa.pl.redbugs.model.Route;
 import com.isa.pl.redbugs.model.Stop;
 import com.isa.pl.redbugs.model.Vehicle;
 import com.isa.pl.redbugs.model.VehicleType;
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class InitDataService {
-
-    static ArrayList<Stop> stops = new ArrayList<>();
-    static ArrayList<Vehicle> vehicles = new ArrayList<>();
-    static ArrayList<Route> routes = new ArrayList<>();
-
-    public static void initData() {
+    public static List<Stop> stopsDataList() {
+        List<Stop> stops = new ArrayList<>();
         stops.add(new Stop(1, "Dąbrowa Centrum"));
         stops.add(new Stop(2, "Gdynia Kameliowa"));
         stops.add(new Stop(3, "Paprykowa"));
@@ -23,7 +21,11 @@ public class InitDataService {
         stops.add(new Stop(8, "Dziewicza"));
         stops.add(new Stop(9, "Pogórze - Szkolna"));
         stops.add(new Stop(10, "Dąbka - Zielona"));
+        return stops;
+    }
 
+    public static List<Vehicle> vehiclesDataList() {
+        List<Vehicle> vehicles = new ArrayList<>();
         vehicles.add(new Vehicle(11, "Siedlce - Stara Piła", VehicleType.Bus));
         vehicles.add(new Vehicle(22, "Dworzec Główny - Pruszcz Komarowo", VehicleType.Bus));
         vehicles.add(new Vehicle(33, "Dworzec Główny - Pruszcz Matejki", VehicleType.Bus));
@@ -34,7 +36,11 @@ public class InitDataService {
         vehicles.add(new Vehicle(88, "Dworzec Główny - Pruszcz Matejki", VehicleType.Tram));
         vehicles.add(new Vehicle(99, "Port Lotniczy - Sopot Kamienny", VehicleType.Tram));
         vehicles.add(new Vehicle(10, "Wrzeszcz PKP - Żukowo Urząd Gminy", VehicleType.Tram));
+        return vehicles;
+    }
 
+    public static List<Route> routesDataList() {
+        List<Route> routes = new ArrayList<>();
         routes.add(new Route(21, 11, "Siedlce - Stara Piła", new long[]{1, 3, 5, 7, 2, 8}));
         routes.add(new Route(22, 22, "Port Lotniczy - Sopot Kamienny", new long[]{4, 3, 7, 9, 10}));
         routes.add(new Route(23, 33, "Dworzec Główny - Pruszcz Matejki", new long[]{1, 2, 3, 8}));
@@ -42,9 +48,9 @@ public class InitDataService {
         routes.add(new Route(25, 55, "Dworzec Główny - Pruszcz Komarowo", new long[]{1, 7, 2, 8, 9, 4}));
         routes.add(new Route(26, 66, "Siedlce - Stara Piła 1", new long[]{1, 3, 7, 2, 8}));
         routes.add(new Route(27, 77, "Port Lotniczy - Sopot Kamienny 2", new long[]{2, 4, 3, 7, 9, 10}));
-        routes.add(new Route(28, 88, "Dworzec Główny - Pruszcz Matejki 3", new long[]{1, 2, 3,9, 8}));
+        routes.add(new Route(28, 88, "Dworzec Główny - Pruszcz Matejki 3", new long[]{1, 2, 3, 9, 8}));
         routes.add(new Route(29, 99, "Wrzeszcz PKP - Żukowo Urząd Gminy 4", new long[]{5, 7, 2, 8, 10}));
         routes.add(new Route(30, 100, "Dworzec Główny - Pruszcz Komarowo 5", new long[]{1, 2, 8, 9, 4}));
-
+        return routes;
     }
 }
