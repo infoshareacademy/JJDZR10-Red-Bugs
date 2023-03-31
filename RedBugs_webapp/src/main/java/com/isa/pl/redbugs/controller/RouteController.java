190ec2d476853d94de7f2cda44a13745414a9904
vitharia <sandra.wasilewska8@gmail.com>
2023-03-31 14:03:49 +0200

    stop and route controllers created

---
+++ RedBugs_webapp/src/main/java/com/isa/pl/redbugs/controller/RouteController.java
@@ -0,0 +1,19 @@
+package com.isa.pl.redbugs.controller;
+
+import com.isa.pl.redbugs.service.RouteService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.IOException;
+
+@Controller
+public class RouteController {
+
+    private RouteService routeService;
+
+    @GetMapping("/routes")
+    public String getRoutes() throws IOException {
+        routeService.findAllRoutes();
+        return "routes";
+    }
+}
---
+++ RedBugs_webapp/src/main/java/com/isa/pl/redbugs/controller/StopController.java
@@ -0,0 +1,19 @@
+package com.isa.pl.redbugs.controller;
+
+import com.isa.pl.redbugs.service.StopService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.IOException;
+
+@Controller
+public class StopController {
+
+    private StopService stopService;
+
+    @GetMapping("/stops")
+    public String getStops() throws IOException {
+        stopService.findAllStops();
+        return "stops";
+    }
+}
