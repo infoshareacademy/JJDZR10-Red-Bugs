8b3a1d716183b540b5f7d8cb4c7b1b0bad3a5d95
vitharia <sandra.wasilewska8@gmail.com>
2023-04-04 19:04:29 +0200

    route list updated

--- RedBugs_webapp/src/main/java/com/isa/pl/redbugs/service/InitDataService.java
+++ RedBugs_webapp/src/main/java/com/isa/pl/redbugs/service/InitDataService.java
@@ -39,16 +39,8 @@ public class InitDataService {
 
     public static List<Route> routesDataList() {
         List<Route> routes = new ArrayList<>();
-        routes.add(new Route(21, 11, "Siedlce - Stara Piła", new long[]{1, 3, 5, 7, 2, 8}));
-        routes.add(new Route(22, 22, "Port Lotniczy - Sopot Kamienny", new long[]{4, 3, 7, 9, 10}));
-        routes.add(new Route(23, 33, "Dworzec Główny - Pruszcz Matejki", new long[]{1, 2, 3, 8}));
-        routes.add(new Route(24, 44, "Wrzeszcz PKP - Żukowo Urząd Gminy", new long[]{5, 7, 2, 8, 10}));
-        routes.add(new Route(25, 55, "Dworzec Główny - Pruszcz Komarowo", new long[]{1, 7, 2, 8, 9, 4}));
-        routes.add(new Route(26, 66, "Siedlce - Stara Piła 1", new long[]{1, 3, 7, 2, 8}));
-        routes.add(new Route(27, 77, "Port Lotniczy - Sopot Kamienny 2", new long[]{2, 4, 3, 7, 9, 10}));
-        routes.add(new Route(28, 88, "Dworzec Główny - Pruszcz Matejki 3", new long[]{1, 2, 3, 9, 8}));
-        routes.add(new Route(29, 99, "Wrzeszcz PKP - Żukowo Urząd Gminy 4", new long[]{5, 7, 2, 8, 10}));
-        routes.add(new Route(30, 100, "Dworzec Główny - Pruszcz Komarowo 5", new long[]{1, 2, 8, 9, 4}));
+        routes.add(new Route(4, 4, "Dworzec Główny - Płocka", new String[]{"1013", "1028", "14693", "1200", "1238", "1239", "1241", "1252", "130", "1247", "1717", "1861", "1863", "1865"}));
+        routes.add(new Route(14, 14, "Dworzec Główny - Płocka", new String[]{"1013", "1028", "1248", "1250", "1254", "1247", "1717", "1314", "1865"}));
         return routes;
     }
 
