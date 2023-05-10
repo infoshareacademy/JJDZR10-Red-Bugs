03eba78ba0e4f6e0e5873b917000143eacad3a69
vitharia <sandra.wasilewska8@gmail.com>
2023-05-10 18:20:45 +0200

    unused methods in Schedule deleted

--- RedBugs_webapp/src/main/java/com/isa/pl/redbugs/Application.java
+++ RedBugs_webapp/src/main/java/com/isa/pl/redbugs/Application.java
@@ -2,14 +2,11 @@ package com.isa.pl.redbugs;
 
 import com.isa.pl.redbugs.model.Route;
 import com.isa.pl.redbugs.model.Stop;
-import com.isa.pl.redbugs.service.*;
-import org.springframework.boot.CommandLineRunner;
+import com.isa.pl.redbugs.service.InitDataService;
+import com.isa.pl.redbugs.service.PathFindingService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -29,17 +26,16 @@ public class Application {
 		pathFindingService.calculateShortestRoute(stops, routes, "1314", "14693");
 
 	}
-
-	@Bean
-	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-		return  args -> {
-			System.out.println("Lets inspect!!!!");
-
-			String[] beanNames = ctx.getBeanDefinitionNames();
-			Arrays.sort(beanNames);
-			for (String beanName : beanNames) {
-				System.out.println(beanName);
-			}
-		};
-	}
+//	@Bean
+//	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+//		return  args -> {
+//
+//			String[] beanNames = ctx.getBeanDefinitionNames();
+//			Arrays.sort(beanNames);
+//			for (String beanName : beanNames) {
+//				System.out.println(beanName);
+//			}
+//		};
+//	}
+//
 }
