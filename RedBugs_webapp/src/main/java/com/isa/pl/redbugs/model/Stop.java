0045b9bf2847bfe3cf21db9d1ec4a934c4c4b128
adampawlowski1987 <adampawlowski1987@gmail.com>
2023-05-14 13:23:12 +0200

    Stop class modified

--- RedBugs_webapp/src/main/java/com/isa/pl/redbugs/model/Stop.java
+++ RedBugs_webapp/src/main/java/com/isa/pl/redbugs/model/Stop.java
@@ -1,94 +1,26 @@
 package com.isa.pl.redbugs.model;
 
 import com.isa.pl.redbugs.service.pathfinding.GraphNode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.util.Objects;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
 
 public class Stop implements GraphNode {
     private String stopId;
     private String stopName;
-    private Schedule schedule;
     private double latitude;
     private double longitude;
-
-    public Stop(String stopId, String stopName, Schedule schedule, double latitude, double longitude) {
-        this.stopId = stopId;
-        this.stopName = stopName;
-        this.schedule = schedule;
-        this.latitude = latitude;
-        this.longitude = longitude;
-    }
-
-    public Stop() {
-    }
-
-    @Override
-    public String getStopId() {
-        return stopId;
-    }
-
-    public String getStopName() {
-        return stopName;
-    }
-
-    public double getLatitude() {
-        return latitude;
-    }
-
-    public double getLongitude() {
-        return longitude;
-    }
-
-    public Schedule getSchedule() {
-        return schedule;
-    }
-
-    public void setStopId(String stopId) {
-        this.stopId = stopId;
-    }
-
-    public void setStopName(String stopName) {
-        this.stopName = stopName;
-    }
-
-    public void setSchedule(Schedule schedule) {
-        this.schedule = schedule;
-    }
-
-    public void setLatitude(double latitude) {
-        this.latitude = latitude;
-    }
-
-    public void setLongitude(double longitude) {
-        this.longitude = longitude;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Stop station = (Stop) o;
-
-        if (Double.compare(station.latitude, latitude) != 0) return false;
-        if (Double.compare(station.longitude, longitude) != 0) return false;
-        if (!Objects.equals(stopId, station.stopId)) return false;
-        if (!Objects.equals(stopName, station.stopName)) return false;
-        return Objects.equals(schedule, station.schedule);
-    }
-
-    @Override
-    public int hashCode() {
-        int result;
-        long temp;
-        result = stopId != null ? stopId.hashCode() : 0;
-        result = 31 * result + (stopName != null ? stopName.hashCode() : 0);
-        result = 31 * result + (schedule != null ? schedule.hashCode() : 0);
-        temp = Double.doubleToLongBits(latitude);
-        result = 31 * result + (int) (temp ^ (temp >>> 32));
-        temp = Double.doubleToLongBits(longitude);
-        result = 31 * result + (int) (temp ^ (temp >>> 32));
-        return result;
-    }
+    @Id
+    private Long id;
 
     @Override
     public String toString() {
@@ -98,5 +30,17 @@ public class Stop implements GraphNode {
                 ", latitude=" + latitude +
                 ", longitude=" + longitude +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Stop stop)) return false;
+        return Double.compare(stop.latitude, latitude) == 0 && Double.compare(stop.longitude, longitude) == 0 && Objects.equals(stopId, stop.stopId) && Objects.equals(stopName, stop.stopName) && Objects.equals(id, stop.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(stopId, stopName, latitude, longitude, id);
     }
 }
