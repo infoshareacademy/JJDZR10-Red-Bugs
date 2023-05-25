41f8e0ed022d1706fa049bd2be500e1f4e36532d
adampawlowski1987 <adampawlowski1987@gmail.com>
2023-05-25 18:19:07 +0200

    Data Controller changed

--- RedBugs_webapp/src/main/java/com/isa/pl/redbugs/controller/DataController.java
+++ RedBugs_webapp/src/main/java/com/isa/pl/redbugs/controller/DataController.java
@@ -1,11 +1,14 @@
 package com.isa.pl.redbugs.controller;
+
+import com.isa.pl.redbugs.repository.RouteRepository;
+import com.isa.pl.redbugs.repository.StopRepository;
+import com.isa.pl.redbugs.repository.VehicleRepository;
 import com.isa.pl.redbugs.service.RouteService;
 import com.isa.pl.redbugs.service.StopService;
 import com.isa.pl.redbugs.service.VehicleService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import java.io.IOException;
 
 
 @Controller
@@ -14,23 +17,27 @@ public class DataController {
     private final RouteService routeService;
     private final StopService stopService;
     private final VehicleService vehicleService;
+    private final RouteRepository routeRepository;
+    private final StopRepository stopRepository;
+    private final VehicleRepository vehicleRepository;
 
-    public DataController(RouteService routeService, StopService stopService, VehicleService vehicleService) {
+    public DataController(RouteService routeService, StopService stopService, VehicleService vehicleService, RouteRepository routeRepository, StopRepository stopRepository, VehicleRepository vehicleRepository) {
         this.routeService = routeService;
         this.stopService = stopService;
         this.vehicleService = vehicleService;
+        this.routeRepository = routeRepository;
+        this.stopRepository = stopRepository;
+        this.vehicleRepository = vehicleRepository;
     }
-
 
 
     @GetMapping("/templates/data.html")
-    public String getInitDataServices(Model model) throws IOException {
-        model.addAttribute("routes", routeService.findAllRoutes());
-        model.addAttribute("stops", stopService.findAllStops());
-        model.addAttribute("vehicles", vehicleService.findAllVehicles());
+    public String getInitDataServices(Model model) {
+        model.addAttribute("routes", routeRepository.findAll());
+        model.addAttribute("stops", stopRepository.findAll());
+        model.addAttribute("vehicles", vehicleRepository.findAll());
         return "data";
     }
-
 
 }
 
