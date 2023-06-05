f024cfe4e88602d57bfa36c65dfe2937a6ac286f
vitharia <sandra.wasilewska8@gmail.com>
2023-06-05 18:50:58 +0200

    clean-up // fixing the path finding method

--- RedBugs_webapp/src/main/java/com/isa/pl/redbugs/Application.java
+++ RedBugs_webapp/src/main/java/com/isa/pl/redbugs/Application.java
@@ -1,24 +1,33 @@
 package com.isa.pl.redbugs;
 
+import com.isa.pl.redbugs.model.Route;
+import com.isa.pl.redbugs.model.Stop;
 import com.isa.pl.redbugs.service.InitDataService;
+import com.isa.pl.redbugs.service.PathFindingService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) throws Exception {
+
 		ConfigurableApplicationContext cxt = SpringApplication.run(Application.class, args);
 		InitDataService initService = cxt.getBean(InitDataService.class);
 
-//		List<Stop> stopsList = initService.getStop();
+		List<Stop> stopsList = initService.getStop();
+		List<Route> routesList =
 //		List<Route> routesList = InitDataService.routesDataList();
-//		Set<Stop> stops = new HashSet<>(stopsList);
-//		Set<Route> routes = new HashSet<>(routesList);
-//
-//		PathFindingService pathFindingService = new PathFindingService();
-//		pathFindingService.calculateShortestRoute(stops, routes, "1314", "14693");
+		Set<Stop> stops = new HashSet<>(stopsList);
+		Set<Route> routes = new HashSet<>(routesList);
+
+		PathFindingService pathFindingService = new PathFindingService();
+		pathFindingService.calculateShortestRoute(stops, routes, "1314", "14693");
 
 	}
 }
