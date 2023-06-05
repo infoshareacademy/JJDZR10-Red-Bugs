a5cbd32fb4eb5c67501e080b1d98c981cb4fc545
vitharia <sandra.wasilewska8@gmail.com>
2023-06-05 18:42:42 +0200

    clean-up of DataController

--- RedBugs_webapp/src/main/java/com/isa/pl/redbugs/controller/DataController.java
+++ RedBugs_webapp/src/main/java/com/isa/pl/redbugs/controller/DataController.java
@@ -3,28 +3,18 @@ package com.isa.pl.redbugs.controller;
 import com.isa.pl.redbugs.repository.RouteRepository;
 import com.isa.pl.redbugs.repository.StopRepository;
 import com.isa.pl.redbugs.repository.VehicleRepository;
-import com.isa.pl.redbugs.service.RouteService;
-import com.isa.pl.redbugs.service.StopService;
-import com.isa.pl.redbugs.service.VehicleService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @Controller
 public class DataController {
 
-    private final RouteService routeService;
-    private final StopService stopService;
-    private final VehicleService vehicleService;
     private final RouteRepository routeRepository;
     private final StopRepository stopRepository;
     private final VehicleRepository vehicleRepository;
 
-    public DataController(RouteService routeService, StopService stopService, VehicleService vehicleService, RouteRepository routeRepository, StopRepository stopRepository, VehicleRepository vehicleRepository) {
-        this.routeService = routeService;
-        this.stopService = stopService;
-        this.vehicleService = vehicleService;
+    public DataController(RouteRepository routeRepository, StopRepository stopRepository, VehicleRepository vehicleRepository) {
         this.routeRepository = routeRepository;
         this.stopRepository = stopRepository;
         this.vehicleRepository = vehicleRepository;
