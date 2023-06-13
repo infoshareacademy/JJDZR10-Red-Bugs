a4720adade610d02652b69d709bd818a2f3e54b3
BartoszBalka <bartosz.balka@wp.pl>
2023-06-13 22:39:00 +0200

    Added Pagination

--- RedBugs_webapp/src/main/java/com/isa/pl/redbugs/controller/DataController.java
+++ RedBugs_webapp/src/main/java/com/isa/pl/redbugs/controller/DataController.java
@@ -1,11 +1,17 @@
 package com.isa.pl.redbugs.controller;
 
+import com.isa.pl.redbugs.model.Route;
+import com.isa.pl.redbugs.model.Stop;
+import com.isa.pl.redbugs.model.Vehicle;
 import com.isa.pl.redbugs.repository.RouteRepository;
 import com.isa.pl.redbugs.repository.StopRepository;
 import com.isa.pl.redbugs.repository.VehicleRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class DataController {
@@ -22,10 +28,19 @@ public class DataController {
 
 
     @GetMapping("/templates/data.html")
-    public String getInitDataServices(Model model) {
-        model.addAttribute("routes", routeRepository.findAll());
-        model.addAttribute("stops", stopRepository.findAll());
-        model.addAttribute("vehicles", vehicleRepository.findAll());
+    public String getInitDataServices(Model model, @RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size) {
+        // Pobierz stronę danych z repozytorium, używając parametrów paginacji
+        Page<Route> routesPage = routeRepository.findAll(PageRequest.of(page, size));
+        Page<Stop> stopsPage = stopRepository.findAll(PageRequest.of(page, size));
+        Page<Vehicle> vehiclesPage = vehicleRepository.findAll(PageRequest.of(page, size));
+
+        // Przekaż informacje dotyczące paginacji i fragmentu danych do modelu
+        model.addAttribute("routes", routesPage.getContent());
+        model.addAttribute("stops", stopsPage.getContent());
+        model.addAttribute("vehicles", vehiclesPage.getContent());
+        model.addAttribute("currentPage", routesPage.getNumber());
+        model.addAttribute("totalPages", routesPage.getTotalPages());
+
         return "data";
     }
 
