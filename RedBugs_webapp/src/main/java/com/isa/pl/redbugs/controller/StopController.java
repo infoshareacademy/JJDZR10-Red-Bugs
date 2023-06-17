e282c71ae8e0d7441c7169f0ed5ae7ceb1078cf5
adampawlowski1987 <adampawlowski1987@gmail.com>
2023-06-17 22:03:34 +0200

    Deleting stop functionalilyty added

--- RedBugs_webapp/src/main/java/com/isa/pl/redbugs/controller/StopController.java
+++ RedBugs_webapp/src/main/java/com/isa/pl/redbugs/controller/StopController.java
@@ -1,21 +1,46 @@
 package com.isa.pl.redbugs.controller;
 
-import com.isa.pl.redbugs.repository.StopRepository;
+import com.isa.pl.redbugs.model.Stop;
+import com.isa.pl.redbugs.service.StopService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.io.IOException;
+import java.util.List;
 
 @Controller
 public class StopController {
 
-    private final StopRepository stopRepository;
+    private final StopService stopService;
 
-    public StopController(StopRepository stopRepository) {
-        this.stopRepository = stopRepository;
+    public StopController(StopService stopService) {
+        this.stopService = stopService;
     }
 
     @GetMapping("/stops")
-    public String getStops() {
-        stopRepository.findAll();
+    public String getStops(Model model) throws IOException {
+        List<Stop> stops = stopService.findAllStops();
+        model.addAttribute("stops", stops);
         return "stops";
     }
+
+    @GetMapping("/addStop")
+    public String addStopForm(Model model) {
+        model.addAttribute("stop", new Stop());
+        return "addStop";
+    }
+
+    @PostMapping("/addStop")
+    public String addStop(@ModelAttribute("stop") Stop stop) {
+        try {
+            stopService.saveStop(stop);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return "redirect:/stops";
+    }
 }
+
--- RedBugs_webapp/src/main/java/com/isa/pl/redbugs/service/StopService.java
+++ RedBugs_webapp/src/main/java/com/isa/pl/redbugs/service/StopService.java
@@ -1,8 +1,27 @@
 package com.isa.pl.redbugs.service;
 
-import org.springframework.stereotype.Component;
+import com.isa.pl.redbugs.model.Stop;
+import com.isa.pl.redbugs.repository.StopRepository;
+import org.springframework.stereotype.Service;
 
-@Component
+import java.io.IOException;
+import java.util.List;
+
+@Service
 public class StopService {
 
+    private final StopRepository stopRepository;
+
+    public StopService(StopRepository stopRepository) {
+        this.stopRepository = stopRepository;
+    }
+
+    public List<Stop> findAllStops() {
+        return stopRepository.findAll();
+    }
+
+    public void saveStop(Stop stop) throws IOException {
+        stopRepository.save(stop);
+    }
 }
+
