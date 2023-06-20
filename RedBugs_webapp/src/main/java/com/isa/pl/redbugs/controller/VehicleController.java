15c0d2259ffa7d2904ff4c48f1d74b50c58f28e2
vitharia <sandra.wasilewska8@gmail.com>
2023-06-20 19:41:14 +0200

    edit-car created // button with link to edit works

--- RedBugs_webapp/src/main/java/com/isa/pl/redbugs/controller/VehicleController.java
+++ RedBugs_webapp/src/main/java/com/isa/pl/redbugs/controller/VehicleController.java
@@ -1,8 +1,17 @@
 package com.isa.pl.redbugs.controller;
 
+import com.isa.pl.redbugs.model.Vehicle;
 import com.isa.pl.redbugs.repository.VehicleRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class VehicleController {
@@ -14,8 +23,18 @@ public class VehicleController {
     }
 
     @GetMapping("/vehicles")
-    public String getVehicles() {
-        vehicleRepository.findAll();
+    public String getVehicles(Model model) {
+        List<Vehicle> vehicles = vehicleRepository.findAll();
+        model.addAttribute("vehicles", vehicles);
         return "vehicles";
     }
+
+
+    @GetMapping("/vehicles/{vehicleId}")
+    public String getVehicleById(@PathVariable("vehicleId") Long vehicleId, Model model) {
+        Vehicle vehicle = vehicleRepository.findById(vehicleId).get();
+        model.addAttribute("vehicle", vehicle);
+        return "edit-vehicle";
+    }
+
 }
