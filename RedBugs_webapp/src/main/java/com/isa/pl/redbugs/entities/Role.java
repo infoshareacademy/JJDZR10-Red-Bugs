323367a1544a9ba2bc7d2ba1f811292b37006fd9
WladyslawM <uladzislaumikhno@gmail.com>
2023-06-30 00:23:20 +0200

    create class Role

---
+++ RedBugs_webapp/src/main/java/com/isa/pl/redbugs/entities/Role.java
@@ -0,0 +1,19 @@
+package com.isa.pl.redbugs.entities;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+}
