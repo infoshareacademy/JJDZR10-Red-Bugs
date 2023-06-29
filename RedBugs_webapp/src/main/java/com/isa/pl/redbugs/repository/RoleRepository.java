0273587df7b3112d09cdb33aa290885f4084c434
WladyslawM <uladzislaumikhno@gmail.com>
2023-06-30 00:40:34 +0200

    add @ to RoleRepository

--- RedBugs_webapp/src/main/java/com/isa/pl/redbugs/repository/RoleRepository.java
+++ RedBugs_webapp/src/main/java/com/isa/pl/redbugs/repository/RoleRepository.java
@@ -2,9 +2,11 @@ package com.isa.pl.redbugs.repository;
 
 import com.isa.pl.redbugs.entities.Role;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface RoleRepository extends CrudRepository<Role, Integer> {
     Optional<Role> findByName(String name);
 }
