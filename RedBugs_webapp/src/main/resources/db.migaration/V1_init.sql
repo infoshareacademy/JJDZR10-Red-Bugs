dc850ef46bad9f759e6cb98786d8115b7a683df8
WladyslawM <uladzislaumikhno@gmail.com>
2023-06-29 23:51:40 +0200

    create sql file tabel user_roles

--- RedBugs_webapp/src/main/resources/db.migaration/V1_init.sql
+++ RedBugs_webapp/src/main/resources/db.migaration/V1_init.sql
@@ -1,4 +1,4 @@
-create table users (
+CREATE TABLE users (
     id bigserial,
     username varchar(30) not null unique,
     password varchar(30) not null ,
@@ -6,9 +6,17 @@ create table users (
     primary key (id)
 );
 
-create table roles (
+CREATE TABLE roles (
     id serial,
     name varchar(50) not null ,
     primary key (id)
+);
+
+CREATE TABLE  user_roles (
+    user_id bigint not null ,
+    role_id int not null ,
+    primary key (user_id, role_id),
+    foreign key (user_id) references users (id),
+    foreign key (role_id) references roles (id)
 );
 
