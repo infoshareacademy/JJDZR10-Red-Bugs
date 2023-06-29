021ffea7954b4682a669ffebe6900fa5b05fdcb4
WladyslawM <uladzislaumikhno@gmail.com>
2023-06-30 00:40:02 +0200

    create UserService

--- RedBugs_webapp/src/main/java/com/isa/pl/redbugs/service/UserService.java
+++ RedBugs_webapp/src/main/java/com/isa/pl/redbugs/service/UserService.java
@@ -1,6 +1,48 @@
 package com.isa.pl.redbugs.service;
 
-public class UserService {
+import com.isa.pl.redbugs.entities.User;
+import com.isa.pl.redbugs.repository.RoleRepository;
+import com.isa.pl.redbugs.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-    public void createUser(){}
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService {
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+
+    public Optional<User> findByUserName(String username) {
+        return userRepository.findByUserName(username);
+    }
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = findByUserName(username).orElseThrow(() -> new UsernameNotFoundException(
+                String.format("Not found user '%s'", username)
+        ));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                user.getRoles()
+                        .stream()
+                        .map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList())
+        );
+    }
+
+    public void createNewUser(User user) {
+        user.setRoles(List.of(roleRepository.findByName("ROLE_USER").get()));
+        userRepository.save(user);
+    }
 }
